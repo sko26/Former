d3783bbd4aec485f31784e841e23a1ab85cf2430
Konstantin <sko26.alex@gmail.com>
2017-02-23 15:02:32 +0300

    simple window

---
+++ src/Former.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Former extends JFrame {
+    public Former()
+    {
+      setSize(400, 300);
+      getContentPane().setLayout(null);
+//        this.add(getJLabel(), null);
+//        this.add(getJTextField(), null);
+//        this.add(getJButton(), null);
+      setTitle("make XML form");
+    }
+
+//    private javax.swing.JLabel getJLabel() {
+//        if(jLabel == null) {
+//            jLabel = new javax.swing.JLabel();
+//            jLabel.setBounds(34, 49, 53, 18);
+//            jLabel.setText("Name:");
+//        }
+//        return jLabel;
+//    }
+
+//    private javax.swing.JTextField getJTextField() {
+//        if(jTextField == null) {
+//            jTextField = new javax.swing.JTextField();
+//            jTextField.setBounds(96, 49, 160, 20);
+//        }
+//        return jTextField;
+//    }
+//
+//    private javax.swing.JButton getJButton() {
+//        if(jButton == null) {
+//            jButton = new javax.swing.JButton();
+//            jButton.setBounds(103, 110, 71, 27);
+//            jButton.setText("OK");
+//        }
+//        return jButton;
+//    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Former former = new Former();
+        former.setVisible(true);
+    }
+}
